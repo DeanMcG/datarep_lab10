cbd2789a7953d69c44c7a1f09eda147c97f1e9b9
Dean0 McGowan <G00369547@gmit.ie>
2020-12-28 14:37:21 +0000

    lab complete

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -73,6 +73,16 @@ app.get('/api/movies/:id', (req,res)=>{
     })
 })
 
+app.put('/api/movies/:id', (req, res)=>{
+    console.log("Updata Movie: "+req.params.id);
+    console.log(req.body);
+
+    MovieModel.findByIdAndUpdate(req.params.id,req.body, {new:true},
+        (err,data)=>{
+            res.send(data);
+        })
+})
+
 //.post runs when the movie has been received
 app.post('/api/movies', (req, res) => {
     console.log('Movie Received!');
--- src/components/movieItem.js
+++ src/components/movieItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from '../App';
 import Card from 'react-bootstrap/Card';
+import {Link} from 'react-router-dom';
 
 
 export class MovieItem extends React.Component {
@@ -18,6 +19,7 @@ export class MovieItem extends React.Component {
                             </footer>
                         </blockquote>
                     </Card.Body>
+                    <Link to={"/edit/"+ this.props.movie._id} className="btn btn-primary">Edit</Link>
                 </Card>
 
 
